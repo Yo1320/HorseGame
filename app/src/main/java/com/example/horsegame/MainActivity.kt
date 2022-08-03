a4541bd5e767c0a059f4735e34e2afddfd99588c
Yo1320 <luised933@gmail.com>
2022-08-03 18:58:47 -0500

    Se quita el Action Bar

--- app/src/main/java/com/example/horsegame/MainActivity.kt
+++ app/src/main/java/com/example/horsegame/MainActivity.kt
@@ -7,5 +7,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initScreenGame()
+    }
+    private fun initScreenGame(){
+        setSizeBoard()
+        hide_message()
+    }
+    private fun setSizeBoard(){
+
+    }
+    Private fun hide_message(){
+        var lyMessage
     }
 }
