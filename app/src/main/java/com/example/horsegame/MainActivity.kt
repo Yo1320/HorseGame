97f6089e101dcad276ed0bea63ba882e6a9f5f6d
Yo1320 <luised933@gmail.com>
2022-08-04 20:21:57 -0500

    Se agrega la posicion aleatoria, matriz y limitaciones de movimentos del usuario

--- app/src/main/java/com/example/horsegame/MainActivity.kt
+++ app/src/main/java/com/example/horsegame/MainActivity.kt
@@ -8,13 +8,78 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TableRow
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+    private var cellSelected_x = 0
+    private var cellSelected_y = 0
+    private lateinit var board: Array<IntArray>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         initScreenGame()
+        resetBoard()
+        setFirstPosition()
+    }
+    fun checkChellClicked(v: View){
+        var name = v.tag.toString()
+        var x = name.subSequence(1,2).toString().toInt()
+        var y = name.subSequence(2,3).toString().toInt()
+        checkCell(x,y)
+    }
+    private fun checkCell(x: Int, y: Int){
+        var dif_x = x - cellSelected_x
+        var dif_y = y - cellSelected_y
+        var checkTrue = false
+        if (dif_x == 1 && dif_y == 2) checkTrue = true
+        if (dif_x == 1 && dif_y == -2) checkTrue = true
+        if (dif_x == 2 && dif_y == 1) checkTrue = true
+        if (dif_x == 2 && dif_y == -1) checkTrue = true
+        if (dif_x == -1 && dif_y == 2) checkTrue = true
+        if (dif_x == -1 && dif_y == -2) checkTrue = true
+        if (dif_x == -2 && dif_y == 1) checkTrue = true
+        if (dif_x == -2 && dif_y == -1) checkTrue = true
+
+        if (board[x][y] == 1 ) checkTrue = false
+        if(checkTrue) selectCell(x,y)
+    }
+    private fun resetBoard(){
+        // 0 Esta libre.
+        // 1 Casilla marcada.
+        // 2 Es un bonus.
+        // 9 Es una opcion del movimiento actual.
+        board = arrayOf(
+            intArrayOf(0,0,0,0,0,0,0,0),
+            intArrayOf(0,0,0,0,0,0,0,0),
+            intArrayOf(0,0,0,0,0,0,0,0),
+            intArrayOf(0,0,0,0,0,0,0,0),
+            intArrayOf(0,0,0,0,0,0,0,0),
+            intArrayOf(0,0,0,0,0,0,0,0),
+            intArrayOf(0,0,0,0,0,0,0,0),
+            intArrayOf(0,0,0,0,0,0,0,0)
+        )
+    }
+    private fun setFirstPosition(){
+        var x = 0
+        var y = 0
+        x = (0..7).random()
+        y = (0..7).random()
+        cellSelected_x = x
+        cellSelected_y = y
+        selectCell(x,y)
+    }
+    private fun selectCell(x: Int, y: Int){
+        board[x][y] = 1
+        paintHorseCell(cellSelected_x, cellSelected_y, "previus_cell")
+        cellSelected_x = x
+        cellSelected_y = y
+        paintHorseCell(x,y, "selected_cell")
+    }
+    private fun paintHorseCell(x: Int, y: Int, color: String){
+        var iv: ImageView = findViewById(resources.getIdentifier("c$x$y", "id", packageName))
+        iv.setBackgroundColor(ContextCompat.getColor(this, resources.getIdentifier(color,"color", packageName)))
+        iv.setImageResource(R.drawable.icon)
     }
     private fun initScreenGame(){
         setSizeBoard()
