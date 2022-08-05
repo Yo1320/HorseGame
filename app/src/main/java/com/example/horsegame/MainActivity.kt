f8228c6d5a49faded37c263868681005016b7c06
Yo1320 <luised933@gmail.com>
2022-08-04 19:10:21 -0500

    Se redimensiona el tablero

--- app/src/main/java/com/example/horsegame/MainActivity.kt
+++ app/src/main/java/com/example/horsegame/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.example.horsegame
 
+import android.graphics.Point
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.TypedValue
+import android.view.View
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TableRow
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,9 +21,30 @@ class MainActivity : AppCompatActivity() {
         hide_message()
     }
     private fun setSizeBoard(){
+        var iv: ImageView
 
+        val display = windowManager.defaultDisplay
+        var size = Point()
+        display.getSize(size)
+        val width = size.x
+
+        var width_dp = (width / getResources().getDisplayMetrics().density)
+
+        var lateralMarginDP = 0
+        val width_cell = (width_dp - lateralMarginDP) /8
+        val height_cell = width_cell
+        for (i in 0..7){
+            for (j in 0..7){
+                iv = findViewById(resources.getIdentifier("c$i$j", "id", packageName))
+                var height = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, height_cell, getResources().getDisplayMetrics()).toInt()
+                var width = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, width_cell, getResources().getDisplayMetrics()).toInt()
+
+                iv.setLayoutParams(TableRow.LayoutParams(width, height))
+            }
+        }
     }
-    Private fun hide_message(){
-        var lyMessage
+    private fun hide_message(){
+        var lyMessage = findViewById<LinearLayout>(R.id.lyMessage)
+        lyMessage.visibility = View.INVISIBLE
     }
 }
